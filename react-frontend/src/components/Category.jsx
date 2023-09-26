4529b887a17d53a7a577f89644427990d3e3e198
lungileN2022 <lungile.nkabinde@takealot.com>
2023-09-26 11:21:14 +0200

    Updated home page

--- react-frontend/src/components/Category.jsx
+++ react-frontend/src/components/Category.jsx
@@ -34,12 +34,36 @@ const Category = () => {
 
                     ))
                 }
-
-
-
-
-
             </div>
+            <div className="container flex gap-6 justify-between">
+
+                    <div className="card rounded-xl h-52 w-48 items-center justify-center pt-5 relative">
+                        <img className="min-w-full min-h-full" src="../src/assets/Furniture-min.png" alt="furniture"></img>
+                        <h1 className="w-full text-2xl font-bold absolute top-0 left-0 right-auto bottom-auto text-center">Furniture</h1>
+                    </div>
+
+                    <div className="card rounded-xl h-52 w-48 items-center justify-center pt-5 relative">
+                        <h1 className="w-full text-2xl font-bold absolute top-0 left-0 right-auto bottom-auto text-center">Books</h1>
+                        <img className="min-w-full min-h-full" src="../src/assets/books-min.png" alt="books"></img>
+                    </div>
+
+                    <div className="card rounded-xl h-52 w-48 items-center justify-center pt-5 relative">
+                        <h1 className="w-full text-2xl font-bold absolute top-0 left-0 right-auto bottom-auto text-center">Tech</h1>
+                        <img className="min-w-full min-h-full" src="../src/assets/tech-min.png" alt="tech"></img>
+                    </div>
+
+                    <div className="card rounded-xl h-52 w-48 items-center justify-center pt-5 relative">
+                        <h1 className="w-full text-2xl font-bold absolute top-0 left-0 right-auto bottom-auto text-center">Travel</h1>
+                        <img className="min-w-full min-h-full" src="../src/assets/travel.png" alt="travel"></img>
+                    </div>
+
+                    <div className="card rounded-xl h-52 w-48 items-center justify-center pt-5 relative">
+                        <h1 className="w-full text-2xl font-bold absolute top-0 left-0 right-auto bottom-auto text-center">Sneakers</h1>
+                        <img className="min-w-full min-h-full" src="../src/assets/sneakers.png" alt="sneakers"></img>
+                    </div>
+
+
+                </div>
         </section>
     )
 }
--- react-frontend/src/components/Products.jsx
+++ react-frontend/src/components/Products.jsx
@@ -23,6 +23,8 @@ const Products = () => {
             <h1 className="container font-extrabold text-3xl py-8">
                 Todays Best Deals for you
             </h1>
+            
+            
             <div className="container grid grid-cols-3 gap-6">
                 {/*
                     prodList.map(prod => (
@@ -34,15 +36,48 @@ const Products = () => {
 
                     ))*/
                 }
+                
 
                 <div class="product-item">
                     <div class="product-image">
-                        <img src="../src/assets/Furniture-min.png" alt="Product Image" />
+                        <img src="../src/assets/nikeSneaker.png" alt="Product Image" />
                     </div>
                     <div class="product-details">
-                        <h2 class="product-title">Product Title</h2>
-                        <p class="product-description">Product Description Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <span class="product-price">$19.99</span>
+                        <h2 class="product-title">Nike Air Max 97 SE</h2>
+                        <p class="product-description">Women's Shoes</p>
+                        <span class="product-price">R2 599,97</span>
+                        <div class="product-rating">
+                            <span class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
+                            <span class="reviews">(100 reviews)</span>
+                        </div>
+                        <span class="favorite">&#9825;</span>
+                    </div>
+                </div>
+
+                <div class="product-item">
+                    <div class="product-image">
+                        <img src="../src/assets/suitcaseCover.png" alt="Product Image" />
+                    </div>
+                    <div class="product-details">
+                        <h2 class="product-title">Luggage Cover M</h2>
+                        <p class="product-description">Spinner 69cm Lycra</p>
+                        <span class="product-price">R599,99</span>
+                        <div class="product-rating">
+                            <span class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
+                            <span class="reviews">(12 reviews)</span>
+                        </div>
+                        <span class="favorite">&#9825;</span>
+                    </div>
+                </div>
+
+                <div class="product-item">
+                    <div class="product-image">
+                        <img src="../src/assets/instax.png" alt="Product Image" />
+                    </div>
+                    <div class="product-details">
+                        <h2 class="product-title">Fujifilm Instax Mini 12 Instant Film Camera</h2>
+                        <p class="product-description">3 colours available </p>
+                        <span class="product-price">R1 799,99</span>
                         <div class="product-rating">
                             <span class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
                             <span class="reviews">(5 reviews)</span>
@@ -50,8 +85,105 @@ const Products = () => {
                         <span class="favorite">&#9825;</span>
                     </div>
                 </div>
+                
             </div>
+
+            <hr></hr>
+
+            <h1 className="container font-extrabold text-3xl py-8">
+                Mostly browsed products
+            </h1>
+            
+
+            {
+                <div className='product-categories'>
+
+                    {/* --- */}
+
+                    <div className="product-item">
+                        <div className="product-image">
+                            <img src="../src/assets/macbook.png" alt="Product Image" />
+                        </div>
+
+                        <div className="product-details">
+                            <h2 className="product-title"> MacBook Pro M1 16"</h2>
+                            <p className="product-description"> 256 GB, 8 core GPU, 8 GB</p>
+                            <span className="product-price"> R 42 000.00</span>
+                            <div className="product-rating">
+                                <span className="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
+                                <span className="reviews">(5 reviews)</span>
+                            </div>
+                            <span className="favorite">&#9825;</span>
+                        </div>
+
+                    </div>
+
+
+                    <div className="product-item">
+                    <div className="product-image">
+                        <img src="../src/assets/homepod.png" alt="Product Image" />
+                    </div>
+                    <div className="product-details">
+                        <h2 className="product-title"> Homepod </h2>
+                        <p className="product-description"> 5 colors available </p>
+                        <span className="product-price"> R 2 000.00 </span>
+                        <div className="product-rating">
+                            <span className="stars">&#9733;&#9733;&#9733;&#9733;</span>&#9734;
+                            <span className="reviews">(25 reviews)</span>
+                        </div>
+                        <span className="favorite">&#9825;</span>
+                    </div>
+                </div>
+
+
+                <div className="product-item">
+                    <div className="product-image">
+                        <img src="../src/assets/laptopSleeve.png" alt="Product Image" />
+                    </div>
+                    <div className="product-details">
+                        <h2 className="product-title"> Laptop sleeve  </h2>
+                        <p className="product-description"> Organic cotton, fairtrade certified </p>
+                        <span className="product-price"> R 650.00 </span>
+                        <div className="product-rating">
+                            <span className="stars">&#9733;&#9733;&#9733;</span>&#9734;&#9734;
+                            <span className="reviews">(1 review)</span>
+                        </div>
+                        <span className="favorite">&#9825;</span>
+                    </div>
+                </div>
+
+
+                <div className="product-item">
+                    <div className="product-image">
+                        <img src="../src/assets/airpod-max-min.png" alt="Product Image" />
+                    </div>
+                    <div className="product-details">
+                        <h2 className="product-title"> AirPods Max  </h2>
+                        <p className="product-description"> A perfect balance of high-fidelity audio </p>
+                        <span className="product-price"> R 6 050.00 </span>
+                        <div className="product-rating">
+                            <span className="stars">&#9733;&#9733;&#9733;&#9733;</span>&#9734;
+                            <span className="reviews">(6 reviews)</span>
+                        </div>
+                        <span className="favorite">&#9825;</span>
+                    </div>
+                </div>
+
+               
+
+
+
+                    {/* ---- */}
+
+                </div>
+            }
+
+            
+
         </section>
+
+        
+
     )
 }
 
